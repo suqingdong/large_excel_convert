69b38a4b2485a7c70f47b827170c7125a3150968
suqingdong <1078595229@qq.com>
2024-11-15 14:09:10 +0800

    compatible with the new version

--- large_excel_convert/core.py
+++ large_excel_convert/core.py
@@ -8,12 +8,24 @@ class ExcelParser(object):
         self.sheet = sheet
         self.chunksize = chunksize
         self.zip_handle = zipfile.ZipFile(file)
+
         self.namespace = self.get_namespace()
+        self.namespaces = {None: self.namespace}
+
+        self.shared_strings = self.get_shared_strings()
+
         self.tag = f'{{{self.namespace}}}{tag}' if self.namespace else tag
 
     def get_namespace(self):
         tree = etree.parse(self.zip_handle.open('xl/workbook.xml'))
         return list(tree.getroot().nsmap.values())[0]
+    
+    def get_shared_strings(self):
+        if 'xl/sharedStrings.xml' not in self.zip_handle.namelist():
+            return {}
+        tree = etree.parse(self.zip_handle.open('xl/sharedStrings.xml'))
+        elements = tree.findall('si/t', namespaces=self.namespaces)
+        return dict((enumerate((e.text or '').strip() for e in elements)))
 
     def rows(self):
         xml_parser = etree.XMLPullParser(events=('end',), tag=self.tag)
@@ -25,13 +37,23 @@ class ExcelParser(object):
         yield from self.read_events(xml_parser)
 
     def read_events(self, xml_parser):
-        for action, elem in xml_parser.read_events():
-            yield list(self.get_row_values(elem))
-            elem.clear()
+        for action, row_element in xml_parser.read_events():
+            values = list(self.get_row_values(row_element))
+            yield values
+            row_element.clear()
 
+    def get_row_values(self, row_element):
+        for cell in row_element.findall('c', namespaces=self.namespaces):
+            cell_type = cell.get('t')
+            v = cell.find('v', namespaces=self.namespaces)
 
-    @staticmethod
-    def get_row_values(elem):
-        for child in elem.getchildren():
-            text = '|'.join(t.strip().replace('\n', ';') for n, t in enumerate(child.itertext()) if t.strip())
-            yield text
+            if cell_type == 's':
+                value = self.shared_strings[int(v.text)]
+            elif cell_type == 'inlineStr':
+                value = ''.join(cell.itertext())
+            else:
+                value = v.text if v is not None else ''
+
+            value = value.strip().replace('\n', ';')
+
+            yield value
